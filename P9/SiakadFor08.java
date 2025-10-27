efd80824e1ec7998686f63728ac8ebca34b7c7d9
Galuh-design <pramudyaanantagaluh@gmail.com>
2025-10-27 20:09:24 +0700

    Modifikasi percobaan 1

--- P9/SiakadFor08.java
+++ P9/SiakadFor08.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
+
 public class SiakadFor08 {
-public static void main(String[] args) {
-Scanner galuh08 = new Scanner(System.in);
-double nilai08, tertinggi08 = 0, terendah08 = 100;
+    public static void main(String[] args) {
+        Scanner galuh08 = new Scanner(System.in);
+        double nilai08, tertinggi08 = 0, terendah08 = 100;
+        int jSiswaLulus = 0, jSiswaTdkLulus = 0;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai08 = galuh08.nextDouble();
 
-for (int i = 1; i <= 10; i++) {
-System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
-nilai08 = galuh08.nextDouble();
-    if (nilai08 > tertinggi08) {
-        tertinggi08 = nilai08;
-    }
-    if (nilai08 < terendah08) {
-        terendah08 = nilai08;
-    }
-}
-System.out.println("Nilai tertinggi: "+tertinggi08);
-System.out.println("Nilai terendah: "+terendah08);
+            if (nilai08 > tertinggi08) {
+                tertinggi08 = nilai08;
+            }
+            if (nilai08 < terendah08) {
+                terendah08 = nilai08;
+            }
+            if (nilai08 >= 60) {
+                jSiswaLulus++;
+            }
+            if (nilai08 < 60) {
+                jSiswaTdkLulus++;
+            }
 
-}
-    
+        }
+
+        System.out.println("Nilai tertinggi: " + tertinggi08);
+        System.out.println("Nilai terendah: " + terendah08);
+        System.out.println("Jumlah mahasiswa lulus: " + jSiswaLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jSiswaTdkLulus);
+    }
+
 }
